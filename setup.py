f98389ca7414d5306fd2b007d2aee8c43da6ce69
Matt <matthew.roy@manchester.ac.uk>
2016-04-03 15:01:19 +0100

    Setup changed

--- setup.py
+++ setup.py
@@ -1,29 +1,36 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def read(fname):
 	return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+with open(path.join(here, 'README.MD'), encoding='utf-8') as f:
+	long_description = f.read()
 
-setup(name = "pyCM",
-	version = "1.0",
-	description = "Python and VTK contour method toolkit",
-	author = "M J Roy",
-	author_email = "matthew.roy@manchester.ac.uk",
-	url = "http://www.contour-method.manchester.ac.uk",
+setup(name = 'pyCM',
+	version = '0.1.0.dev0',
+	description = 'Python contour method toolkit',
+	long_description = long_description,
+	url = 'https://github.com/majroy/pyCM',
+	author = 'M J Roy',
+	author_email = 'matthew.roy@manchester.ac.uk',
+
+	classifiers=[
+		'Development Status :: 2 - Pre-Alpha',
+		'Environment :: Win32 (MS Windows)',
+		'Topic :: Scientific/Engineering :: Visualization',
+		'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+		'Operating System :: Microsoft :: Windows :: Windows 7',
+		'Programming Language :: Python :: 2.7',
+		'Framework :: Sphinx',
+		'Intended Audience :: End Users/Desktop',
+		'Natural Language :: English',
+		],
+
+	install_requires=['vtk','numpy','scipy','Tkinter'],
+	license = 'Creative Commons Attribution-Noncommercial-Share Alike license'
 	keywords = "residual stress contour method VTK",
 	packages=['pyCM'],
 	package_data = {'pyCM' : ['meta/*',] },
-	long_description = read('README'),
-	classifiers=[
-		"Development Status :: 2 - Pre-Alpha",
-		"Environment :: Win32 (MS Windows)",
-		"Topic :: Scientific/Engineering :: Visualization",
-		"License :: Other/Proprietary License",
-		"Operating System :: Microsoft :: Windows :: Windows 7",
-		"Programming Language :: Python :: 2.7",
-		"Framework :: IDLE",
-		"Intended Audience :: End Users/Desktop",
-		"Natural Language :: English",
-		],
+
 	)
