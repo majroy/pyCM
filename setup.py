32fa6d33510d4017c496c49b2617943ce7cbd113
Matt <matthew.roy@manchester.ac.uk>
2016-11-26 16:49:54 +0000

    Changed setup.py settings

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(name = 'pyCM',
 		'Natural Language :: English',
 		],
 
-	install_requires=['vtk','numpy','scipy','pyQt4'],
+	install_requires=['vtk','numpy','scipy','PyQt4'],
 	license = 'Creative Commons Attribution-Noncommercial-Share Alike license',
 	keywords = 'residual stress contour method VTK',
 	packages=['pyCM'],
